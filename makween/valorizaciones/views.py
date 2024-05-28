748032cfb36f9e11e2724ee94c6eb41e2a2ed862
Vina del Mar <vina@duoc.acad>
2024-05-27 21:12:59 -0400

    no se que hice, pero messi

--- makween/valorizaciones/views.py
+++ makween/valorizaciones/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from .models import Alumno,Genero
+
+
+def index (request):
+    valorizaciones= valorizaciones.objects.all()
+    context={"alumnos":valorizaciones}
+    return render(request,'valorizaciones/template/index.html', context)
